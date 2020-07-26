0e1db6083cf3129a6ad06f27bfd260947e71e6a1
Stormy <stormcloud.enterprises@gmail.com>
2020-07-26 16:03:09 -0700

    first commit - nothing in it yet - user accounts - first build/run

---
+++ HolmanBerg/HolmanBerg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HolmanBerg.Startup))]
+namespace HolmanBerg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
